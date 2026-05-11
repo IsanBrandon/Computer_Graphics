29d09ded9a0c7e1514e9f630f2cfd985dff57165
IsanBrandon <hyeok831@gmail.com>
2026-05-11 18:58:28 +0900

    ex03-04

--- exercises/ex03_reshape_callback.cpp
+++ exercises/ex03_reshape_callback.cpp
@@ -5,38 +5,16 @@
 #define _WINDOW_WIDTH 300
 #define _WINDOW_HEIGHT 300
 
-int mode = 0;
-
 void Mydisplay() {
 	glClear(GL_COLOR_BUFFER_BIT);		// 컬러버퍼에 초기화 색을 가함
 	glColor3f(0.5, 0.5, 0.5);			// 색상설정: 회색 (0.5: Red, 0.5: Green, 0.5: Blue)
-	if (mode == 0) {
-		glBegin(GL_POLYGON);				// 사각형 시작 
+	glBegin(GL_POLYGON);				// 사각형 시작 
 		glVertex3f(-0.5, -0.5, 0.0);		// 좌하단 좌표
 		glVertex3f(0.5, -0.5, 0.0);			// 우하단 좌표
 		glVertex3f(0.5, 0.5, 0.0);			// 우상단 좌표
 		glVertex3f(-0.5, 0.5, 0.0);			// 좌상단 좌표
-		glEnd();							// 사각형 끝
-	}
-	else if (mode == 1) {
-		glutWireTeapot(0.6);				// 크기가 0.6인 와이어프레임 티팟 그리기
-	}
+	glEnd();							// 사각형 끝
 	glFlush();							// 버퍼에 있는 내용을 그리도록 함 
-}
-
-void MyKeyboard(unsigned char KeyPressed, int X, int Y) {
-	switch (KeyPressed) {
-	case 'a':
-		mode = (mode + 1) % 2;
-		break;
-	case 'Q':
-		exit(0); break;
-	case 'q':
-		exit(0); break;
-	case 27:							// ESC 키의 ASCII 코드
-		exit(0); break; 
-	}
-	glutPostRedisplay();
 }
 
 void MyReshape(int NewWidth, int NewHeight) {
@@ -49,19 +27,15 @@ void MyReshape(int NewWidth, int NewHeight) {
 		-1.0 * HeightFactor, 1.0 * HeightFactor, -1.0, 1.0);
 }
 
-int main(int arge, char** argv) {		
+int main(int arge, char* argv[]) {		
 	glutInit(&arge, argv);								// GLUT 라이브러리 초기화 
 	glutInitDisplayMode(GLUT_RGB);						// 디스플레이 모드 설정 (RGB)
 	glutInitWindowSize(_WINDOW_WIDTH, _WINDOW_HEIGHT);	// 윈도우 크기 설정
-	glutInitWindowPosition(0, 0);						// 윈도우 위치 설정
+	glutInitWindowPosition(100, 200);					// 윈도우 위치 설정
 	glutCreateWindow("OpenGL Example Drawing");			// 윈도우 생성
 	glClearColor(1.0, 1.0, 1.0, 1.0);					// 초기화 색은 백색
-
-	// 콜백 함수 등록
 	glutDisplayFunc(Mydisplay);							// display함수 지정
-	glutKeyboardFunc(MyKeyboard);						// keyboard함수 지정
 	glutReshapeFunc(MyReshape);							// reshape함수 지정
-
 	glutMainLoop();										// 메인 루프 돌입
 	return 0;											// 메인루프가 끝날 때까지 실행되지 않음 
 }
